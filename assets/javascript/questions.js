b606e14353ca5462476e4de1c242d34cd84f1563
Huiling Wu <huwu@ucdavis.edu>
2018-10-17 19:16:36 -0700

    added skeleton of html and trivia questions

---
+++ assets/javascript/questions.js
@@ -0,0 +1,164 @@
+// Harry Potter questions are from this website: https://www.buzzfeed.com/bullo/potter-heads-only
+// and this website: https://harrypotter.bloomsbury.com/uk/fun-stuff/harry-potter-trivia-challenge/
+var questions = [
+    {
+        q : "When was Harry Potter born?",
+        a1 : "July 1st, 1980",
+        a2 : "July 31st, 1980",
+        a3 : "Auguest 1st, 1980",
+        a4 : "Auguest 31st 1980",
+        correctAns : "July 31st, 1980"
+    },
+    {
+        q : "Which one of these Weasley brothers is the oldest?",
+        a1 : "Charlie",
+        a2 : "Bill",
+        a3 : "Percy",
+        a4 : "Ron",
+        correctAns : "Bill"
+    },
+    {
+        q : "Which is the correct spelling?",
+        a1 : "Huflepuff",
+        a2 : "Hufflepuff",
+        a3 : "Hufflepuf",
+        a4 : "Huflepuf",
+        correctAns : "Hufflepuff"
+    },
+    {
+        q : "Which Horcrux was the second one to be destroyed?",
+        a1 : "The medallion",
+        a2 : "The cup",
+        a3 : "The ring",
+        a4 : "The diadem",
+        correctAns : "The ring"
+    },
+    {
+        q : "Who accompanied Harry to the Yule Ball?",
+        a1 : "Parvati Patill",
+        a2 : "Padma Patil",
+        a3 : "Cho Chang",
+        a4 : "Ginny Weasley",
+        correctAns : "Parvati Patill"
+    },
+    {
+        q : "In the first book, on which floor is the off-limits corridor located?",
+        a1 : "One the second floor",
+        a2 : "On the third floor",
+        a3 : "On the fifth floor",
+        a4 : "On the sixth floor",
+        correctAns : "On the third floor"
+    },
+    {
+        q : "Which ear did Fred Weasley lost?",
+        a1 : "The left ear",
+        a2 : "The right ear",
+        a3 : "Both ears",
+        a4 : "Um, George is the one who lost an ear",
+        correctAns : "Um, George is the one who lost an ear"
+    },
+    {
+        q : "What is Albus Dumbledore's full name?",
+        a1 : "Albus Brian Percival Wulfric Dumbledore",
+        a2 : "Albus Wulfric Percival Brian Dumbledore",
+        a3 : "Albus Percival Wulfric Brian Dumbledore",
+        a4 : "Albus Brian Percival Wulfric Dumbledore",
+        correctAns : "Albus Percival Wulfric Brian Dumbledore"
+    },
+    {
+        q : "What is Luna Lovegood's Patronus?",
+        a1 : "A mouse",
+        a2 : "A hare",
+        a3 : "A cat",
+        a4 : "A ladybug",
+        correctAns : "A hare"
+    },
+    {
+        q : "Which one of these ingredients is NOT used in the making of Polyjuice potion?",
+        a1 : "Lacewing fly",
+        a2 : "Leeches",
+        a3 : "Fluxweed",
+        a4 : "Agapanthus",
+        correctAns : "Agapanthus"
+    },
+    {
+        q : "What is the name of the person that Ron turns into while infiltrating the Ministry of Magic?",
+        a1 : "Reg Cattermole",
+        a2 : "Ray Cuttermile",
+        a3 : "Reg Cottarmule",
+        a4 : "Ray Cottirmale",
+        correctAns : "Reg Cattermole"
+    },
+    {
+        q : "What animal does Rita Skeeter turn into in order to spy on people?",
+        a1 : "A beetle",
+        a2 : "An ant",
+        a3 : "A cockroach",
+        a4 : "A spider",
+        correctAns : "A beetle"
+    },
+    {
+        q : "What is Hermione's last name?",
+        a1 : "Helen",
+        a2 : "Jean",
+        a3 : "Gabriel",
+        a4 : "Judy",
+        correctAns : "Jean"
+    },
+    {
+        q : "What is Parvati Patil's Boggart?",
+        a1 : "A severed hand",
+        a2 : "A ghost",
+        a3 : "A Jack-in-the-Box",
+        a4 : "A mummy",
+        correctAns : "A mummy"
+    },
+    {
+        q : "What shape are Professor McGonagall's glasses?",
+        a1 : "Round",
+        a2 : "Oval",
+        a3 : "Square",
+        a4 : "It's never stated",
+        correctAns : "Square"
+    },
+    {
+        q : "How much is a Galleon worth?",
+        a1 : "12 Sickles",
+        a2 : "17 Sickles",
+        a3 : "23 Sickles",
+        a4 : "29 Sickles",
+        correctAns : "17 Sickles"
+    },
+    {
+        q : "Where is Madam Marsh headed to when she rides the Knight Bus with Harry?",
+        a1 : "Blackwood",
+        a2 : "Abergavenny",
+        a3 : "Gresford",
+        a4 : "Welshpool",
+        correctAns : "Abergavenny"
+    },
+    {
+        q : "What is the symbol for Slytherin house?",
+        a1 : "A lion",
+        a2 : "A badger",
+        a3 : "An Eagle",
+        a4 : "A snake",
+        correctAns : "A snake"
+    },
+    {
+        q : "Who is unanimously elected leader of Dumbledore's Army?",
+        a1 : "Ron Weasley",
+        a2 : "Harry Potter",
+        a3 : "Herminone Granger",
+        a4 : "Neville Longbottom",
+        correctAns : "Harry Potter"
+    },
+    {
+        q : "In Harry Potter and the Prisoner of Azkaban, where does the Knight Bus drop Harry off?",
+        a1 : "King's Cross Station",
+        a2 : "Privet Drive",
+        a3 : "Hogwarts",
+        a4 : "The Leaky Cauldron",
+        correctAns : "The Leaky Cauldron"
+    }
+];
